23824273b55d3b08209840bc774145cf916c9f1a
Nolan405 <nolan.morain@etu.univ-orleans.fr>
2026-03-17 12:25:22 +0100

    debut SPA

---
+++ static/js/app.js
@@ -0,0 +1,28 @@
+const routes = {
+    '#/'                   : Home,
+    '#/characters/'        : Characters,
+    '#/characters/:id/'    : CharacterShow,
+    '#/classes/'           : classes,
+    '#/classes/:id/'       : classeShow,
+    '#/equipements/'       : Equipements,
+    '#/equipements/:id/'   : EquipementShow,
+    '#/races/'             : Races,
+    '#/races/:id/'         : RaceShow
+}
+
+const router = async () => {
+    // const path = location.hash;
+    
+    // if (path.startsWith('#/articles/') && path.split('/').length >= 4) {
+    //     ArticleShow();
+    //     return;
+    // }
+
+    // const view = routes[path];
+    // if (view) {
+    //     view();
+    // } 
+}
+
+window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
